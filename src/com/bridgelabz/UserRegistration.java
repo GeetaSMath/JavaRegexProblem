d3b749718f7e995a331509f425ee795c9b5b1998
GeetaSMath <geetasmath@gmail.com>
2021-11-08 10:31:54 +0530

    Added: Enter Valid LastName and First Charecter should be Capital

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -13,20 +13,26 @@ public class UserRegistration {
             else
                 System.out.println("Please Enter a Valid First name with Only \"One Starting Capital\" latter");
         }
+    public void lastNameCheck(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Last Name of User: " );
+        String  lName = sc.next();
+        boolean check = Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", lName);
+        if (check)
+            System.out.println("Perfect! Last #Name");
+        else
+            System.out.println("Please Enter a Valid Last Name with Only \"One Starting Capital\" latter");
+    }
 
-
-
-
-        public static void main(String[] args) {
+    public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             UserRegistration user = new UserRegistration();
 
             System.out.println("Welcome to User Registration Program");
 
             user.firstNameCheck();
-
-
-
+            user.lastNameCheck();
         }
+
     }
 
