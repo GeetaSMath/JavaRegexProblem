6fbcec62b9e057cd87c224f16d82516184e8bcb7
GeetaSMath <geetasmath@gmail.com>
2021-11-08 10:25:32 +0530

    Added: Enter the Valid Fname first Charecter Should be Capital

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -3,8 +3,30 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class UserRegistration {
-    public static void main(String[] args) {
-        System.out.println("Welcome to solve Regex Problem");
+        Scanner sc = new Scanner(System.in);
+        public void firstNameCheck(){
+            System.out.print("Enter First Name of User: " );
+            String  fName = sc.next();
+            boolean check = Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", fName);
+            if (check)
+                System.out.println("Perfect! First #Name");
+            else
+                System.out.println("Please Enter a Valid First name with Only \"One Starting Capital\" latter");
+        }
+
+
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            UserRegistration user = new UserRegistration();
+
+            System.out.println("Welcome to User Registration Program");
+
+            user.firstNameCheck();
+
+
+
+        }
     }
-}
 
